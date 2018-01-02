6c525751e76d29cf7e347cb82adfc792c28c6d3f
田中 <tnkytnk@hotmail.com>
2018-01-02 21:14:16 +0900

    Wpf host d3d11 by using HwndHost. created by visual studio 2017.

---
+++ WpfApp1/MyViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    class MyViewModel
+    {
+        public string Name { get; } = "Host D3DWindow";
+
+        public class RedrawCommand : ICommand
+        {
+            public event EventHandler CanExecuteChanged;
+
+            public bool CanExecute(object parameter)
+            {
+                return true;
+            }
+
+            public void Execute(object parameter)
+            {
+                WindowHost.GetInstance().Redraw();
+            }
+        }
+        public RedrawCommand MyRedraw { private set; get; } = new RedrawCommand();
+    }
+}
---
+++ WpfApp1/WindowHost.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    using ClassLibrary1;
+    class WindowHost : HwndHost
+    {
+        static WindowHost instance;
+        public static WindowHost GetInstance() { return instance; }
+
+        Class1 wrapper = new Class1();
+
+        public  WindowHost()
+        {
+            wrapper = new Class1();
+            if (instance == null) instance = this;
+        }
+
+        ~WindowHost() { instance = null; }
+
+        protected override HandleRef BuildWindowCore(HandleRef hwndParent)
+        {
+            var border = Parent as Border;
+            var child = wrapper.CreateHostedWindow(hwndParent.Handle, (int)border.Width, (int)border.Height);
+            return new HandleRef(this, child);
+        }
+
+        protected override void DestroyWindowCore(HandleRef hwnd)
+        {
+            if (wrapper == null) return;
+            wrapper.DestroyHostedWindow(hwnd.Handle);
+        }
+
+        public void Redraw()
+        {
+            if (wrapper == null) return;
+            wrapper.Redraw();
+        }
+    }
+}
